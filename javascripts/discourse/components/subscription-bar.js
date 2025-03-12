9180d899888614735025338b5646ff67de9ac558
Toon251 <toon251@gmail.com>
2025-03-12 05:19:25 +0000

    Add fetch current session

--- javascripts/discourse/components/subscription-bar.js
+++ javascripts/discourse/components/subscription-bar.js
@@ -6,5 +6,32 @@ export default class SubscriptionBar extends Component {
     @action
     handleClick() {
         alert("subscription");
+
+        fetchUserSubscription();
+
     }
+
+    async fetchUserSubscription() {
+        try {
+          // Fetch current user name
+          const response1 = await fetch('/session/current.json');
+          const data1 = await response1.json();
+          const user = data1.current_user
+          
+          alert('Username:', user.username)
+
+      
+          // Fetch email
+          /*const response2 = await fetch('https://jsonplaceholder.typicode.com/posts/2');
+          const data2 = await response2.json();
+          console.log('Second fetch result:', data2);
+      
+          // Third fetch
+          const response3 = await fetch('https://jsonplaceholder.typicode.com/posts/3');
+          const data3 = await response3.json();
+          console.log('Third fetch result:', data3);*/
+        } catch (error) {
+          console.error('Error during fetch:', error);
+        }
+      }
 }
