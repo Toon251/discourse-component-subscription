b8f58b6c21fc0f2afa7032f97d60af8b8df04666
Toon251 <toon251@gmail.com>
2025-03-12 04:06:21 +0000

    Rename to subscription-bar

--- javascripts/discourse/api-initializers/subscription-bar.js
+++ javascripts/discourse/api-initializers/subscription-bar.js
@@ -1,5 +1,5 @@
 import { apiInitializer } from 'discourse/lib/api';
-import SubscriptionBar from '../components/subscription';
+import SubscriptionBar from '../components/subscription-bar';
 
 export default apiInitializer('1.14.0', (api) => {
     api.renderInOutlet(settings.plugin_outlet.trim(), SubscriptionBar);
